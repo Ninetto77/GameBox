1ca06feefbd63f558bc9d33076eb7f27d7ee4c5a
Ninetto77 <ndsingkh@stud.eltech.ru>
2024-10-04 20:18:23 +0300

    update: camera shake

---
+++ Assets/Scripts/Camera/ShakeCameraOnWeaponAttack.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Attack.Raycast;
+using DG.Tweening;
+
+public class ShakeCameraOnWeaponAttack: MonoBehaviour
+{
+	private Weapon weapon;
+	private Camera mainCamera;
+
+	private void Start()
+	{
+		mainCamera = Camera.main;
+	}
+
+	private void OnEnable()
+	{
+		weapon = GetComponent<Weapon>();
+		weapon.OnAttackStarted += ShakeCamera;
+	}
+
+	private void OnDisable()
+	{
+		weapon.OnAttackStarted -= ShakeCamera;
+	}
+
+	private void ShakeCamera()
+	{
+		var cameraTransform = mainCamera.transform;
+
+		cameraTransform
+			.DOShakePosition(0.15f, 0.5f, 10, 90f, false, true, ShakeRandomnessMode.Harmonic)
+			.SetEase(Ease.InOutBounce)
+			.SetLink(cameraTransform.gameObject);
+
+			cameraTransform
+		.DOShakeRotation(0.15f, 0.5f, 10, 90f, true, ShakeRandomnessMode.Harmonic)
+		.SetEase(Ease.InOutBounce)
+		.SetLink(cameraTransform.gameObject);
+	}
+}
