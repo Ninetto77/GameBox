a567fd235eeb629b760639010974a58da3726a61
Ninetto77 <ndsingkh@stud.eltech.ru>
2024-11-17 12:23:58 +0300

    Update Zoom.cs

--- Assets/Scripts/Weapon/Obilities/Zoom.cs
+++ Assets/Scripts/Weapon/Obilities/Zoom.cs
@@ -2,67 +2,70 @@ using Attack.Raycast;
 using UnityEngine;
 using Zenject;
 
-public class Zoom: MonoBehaviour
+namespace WeaponObilities
 {
-    public float zoomValue = 60;
-	[Header("Zoom Sensitivity")]
-	public float sensitivityHor = 0.5f;
-	public float sensitivityVer = 0.5f;
-
-	private float oldSensitivityHor;
-	private float oldSensitivityVer;
-
-	private Weapon weapon;
-	private Camera mainCamera;
-	private MouseLook look;
-	private MouseLook playerLook;
-	private bool isZoom = false;
-	[Inject] private UIManager manager;
-	[Inject] private PlayerMoovement player;
-
-	private void Start()
+	public class Zoom : MonoBehaviour
 	{
-		mainCamera = Camera.main;
+		public float zoomValue = 60;
+		[Header("Zoom Sensitivity")]
+		public float sensitivityHor = 0.5f;
+		public float sensitivityVer = 0.5f;
 
-		look = mainCamera.gameObject.GetComponent<MouseLook>();
-		playerLook = player.gameObject.GetComponent<MouseLook>();
+		private float oldSensitivityHor;
+		private float oldSensitivityVer;
 
-		oldSensitivityVer = look.sensitivityVer;
-		oldSensitivityHor = playerLook.sensitivityHor;
-		
-		isZoom = false;
-	}
+		private Weapon weapon;
+		private Camera mainCamera;
+		private MouseLook look;
+		private MouseLook playerLook;
+		private bool isZoom = false;
+		[Inject] private UIManager manager;
+		[Inject] private PlayerMoovement player;
 
-	private void OnEnable()
-	{
-		weapon = GetComponent<Weapon>();
-		weapon.OnZoomMouseClick += ZoomWeapon;
-	}
-
-	private void OnDisable()
-	{
-		weapon.OnZoomMouseClick -= ZoomWeapon;
-	}
-	public void ZoomWeapon()
-	{
-		if (isZoom)
+		private void Start()
 		{
-			mainCamera.fieldOfView = 60;
-			manager.ZoomIcon.enabled = false;
-			manager.AimIcon.enabled = true;
-			look.sensitivityVer = oldSensitivityVer;
-			playerLook.sensitivityHor = oldSensitivityHor;
-		}
-		else
-		{
-			mainCamera.fieldOfView = zoomValue;
-			manager.ZoomIcon.enabled = true;
-			manager.AimIcon.enabled = false;
+			mainCamera = Camera.main;
 
-			look.sensitivityVer = sensitivityVer;
-			playerLook.sensitivityHor = sensitivityHor;
+			look = mainCamera.gameObject.GetComponent<MouseLook>();
+			playerLook = player.gameObject.GetComponent<MouseLook>();
+
+			oldSensitivityVer = look.sensitivityVer;
+			oldSensitivityHor = playerLook.sensitivityHor;
+
+			isZoom = false;
 		}
 
-		isZoom = !isZoom;
+		private void OnEnable()
+		{
+			weapon = GetComponent<Weapon>();
+			weapon.OnZoomMouseClick += ZoomWeapon;
+		}
+
+		private void OnDisable()
+		{
+			weapon.OnZoomMouseClick -= ZoomWeapon;
+		}
+		public void ZoomWeapon()
+		{
+			if (isZoom)
+			{
+				mainCamera.fieldOfView = 60;
+				manager.ZoomIcon.enabled = false;
+				manager.AimIcon.enabled = true;
+				look.sensitivityVer = oldSensitivityVer;
+				playerLook.sensitivityHor = oldSensitivityHor;
+			}
+			else
+			{
+				mainCamera.fieldOfView = zoomValue;
+				manager.ZoomIcon.enabled = true;
+				manager.AimIcon.enabled = false;
+
+				look.sensitivityVer = sensitivityVer;
+				playerLook.sensitivityHor = sensitivityHor;
+			}
+
+			isZoom = !isZoom;
+		}
 	}
 }
